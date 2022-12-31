9e67f024f670302fbec36c8ec457b90872dd4198
Iurii Cherkasov <cherkasov1010@gmail.com>
2022-12-31 20:02:13 +0300

    stages have been done

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"SkillboxDiploma/pkg/result"
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"net/http"
 )
@@ -9,10 +11,20 @@ var localhost = "localhost:8282"
 
 func main() {
 	r := chi.NewRouter()
-	r.HandleFunc("/{category}", handleConnection)
+	r.Get("/", handleConnection)
 	http.ListenAndServe(localhost, r)
 }
 
 func handleConnection(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK"))
+	result := result.GetResultData()
+
+	content, err := json.Marshal(result)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
 }
--- pkg/result/result.go
+++ pkg/result/result.go
@@ -6,21 +6,208 @@ import (
 	"SkillboxDiploma/pkg/billingData"
 	"SkillboxDiploma/pkg/emailData"
 	"SkillboxDiploma/pkg/incidentData"
+	"SkillboxDiploma/pkg/stateCodes"
+	"SkillboxDiploma/pkg/supportData"
 	"SkillboxDiploma/pkg/voiceCall"
 )
 
 type ResultT struct {
-	Status bool       `json:"status"` // True, если все этапы сбора данных прошли успешно, False во всех остальных случаях
-	Data   ResultSetT `json:"data"`   // Заполнен, если все этапы сбора  данных прошли успешно, nil во всех остальных случаях
-	Error  string     `json:"error"`  // Пустая строка, если все этапы сбора данных прошли успешно, в случае ошибки заполнено текстом ошибки
+	Status bool        `json:"status"` // True, если все этапы сбора данных прошли успешно, False во всех остальных случаях
+	Data   *ResultSetT `json:"data"`   // Заполнен, если все этапы сбора  данных прошли успешно, nil во всех остальных случаях
+	Error  string      `json:"error"`  // Пустая строка, если все этапы сбора данных прошли успешно, в случае ошибки заполнено текстом ошибки
 }
 
 type ResultSetT struct {
 	SMS       [][]SMSData.SMSData                `json:"sms"`
 	MMS       [][]MMSData.MMSData                `json:"mms"`
 	VoiceCall []voiceCall.VoiceData              `json:"voice_call"`
-	Email     map[string][][]emailData.EmailData `json: email"`
-	Billing   billingData.BillingData            `json: billing"`
-	Support   []int                              `json: support"`
+	Email     map[string][][]emailData.EmailData `json:"email"`
+	Billing   billingData.BillingData            `json:"billing"`
+	Support   []int                              `json:"support"`
 	Incidents []incidentData.IncidentData        `json:"incident"`
+}
+
+func GetResultData() ResultT {
+	resultT := ResultT{
+		false,
+		nil,
+		"",
+	}
+
+	voiceCall, err := voiceCall.GetData()
+	if err != nil {
+		resultT.Error = err.Error()
+		return resultT
+	}
+
+	resultSetT := ResultSetT{
+		getSMSData(&resultT),
+		getMMSData(&resultT),
+		voiceCall,
+		getEmailData(&resultT),
+		billingData.GetData(),
+		getSupportData(&resultT),
+		getIncidentData(&resultT),
+	}
+
+	if resultT.Error == "" {
+		resultT.Status = true
+		resultT.Data = &resultSetT
+	}
+
+	return resultT
+}
+
+func getSMSData(resultT *ResultT) [][]SMSData.SMSData {
+	smsData, err := SMSData.GetData()
+	if err != nil {
+		resultT.Error = err.Error()
+		return nil
+	}
+
+	for i, s := range smsData {
+		smsData[i].Сountry = stateCodes.GetName(s.Сountry)
+	}
+
+	for i := 0; i < len(smsData); i++ {
+		for j := i; j < len(smsData); j++ {
+			if smsData[i].Provider > smsData[j].Provider {
+				smsData[i], smsData[j] = smsData[j], smsData[i]
+			}
+		}
+	}
+	firstSMSData := smsData
+
+	for i := 0; i < len(smsData); i++ {
+		for j := i; j < len(smsData); j++ {
+			if smsData[i].Сountry > smsData[j].Сountry {
+				smsData[i], smsData[j] = smsData[j], smsData[i]
+			}
+		}
+	}
+	secondSMSData := smsData
+
+	return [][]SMSData.SMSData{firstSMSData, secondSMSData}
+}
+
+func getMMSData(resultT *ResultT) [][]MMSData.MMSData {
+	mmsData, err := MMSData.GetData()
+	if err != nil {
+		resultT.Error = err.Error()
+		return nil
+	}
+
+	for i, m := range mmsData {
+		mmsData[i].Country = stateCodes.GetName(m.Country)
+	}
+
+	for i := 0; i < len(mmsData); i++ {
+		for j := i; j < len(mmsData); j++ {
+			if mmsData[i].Provider > mmsData[j].Provider {
+				mmsData[i], mmsData[j] = mmsData[j], mmsData[i]
+			}
+		}
+	}
+	firstMMSData := mmsData
+
+	for i := 0; i < len(mmsData); i++ {
+		for j := i; j < len(mmsData); j++ {
+			if mmsData[i].Country > mmsData[j].Country {
+				mmsData[i], mmsData[j] = mmsData[j], mmsData[i]
+			}
+		}
+	}
+	secondMMSData := mmsData
+
+	return [][]MMSData.MMSData{firstMMSData, secondMMSData}
+}
+
+func getEmailData(resultT *ResultT) map[string][][]emailData.EmailData {
+	emData, err := emailData.GetData()
+	if err != nil {
+		resultT.Error = err.Error()
+		return nil
+	}
+
+	for i := 0; i < len(emData); i++ {
+		for j := i; j < len(emData); j++ {
+			if emData[i].DeliveryTime > emData[j].DeliveryTime {
+				emData[i], emData[j] = emData[j], emData[i]
+			}
+		}
+	}
+
+	resultMap := make(map[string][][]emailData.EmailData)
+
+	for _, e := range emData {
+		if len(resultMap[e.Country]) == 0 {
+			resultMap[e.Country] = append(resultMap[e.Country], []emailData.EmailData{})
+		}
+	}
+
+	for _, e := range emData {
+		if len(resultMap[e.Country][0]) < 3 {
+			resultMap[e.Country][0] = append(resultMap[e.Country][0], e)
+		}
+	}
+
+	for i := len(emData) - 1; i >= 0; i-- {
+		if len(resultMap[emData[i].Country][0]) < 3 {
+			resultMap[emData[i].Country][0] = append(resultMap[emData[i].Country][0], emData[i])
+		}
+	}
+
+	return resultMap
+}
+
+func getSupportData(resultT *ResultT) []int {
+	suppData, err := supportData.GetData()
+	if err != nil {
+		resultT.Error = err.Error()
+		return nil
+	}
+
+	tikets := 0
+
+	for _, s := range suppData {
+		tikets += s.ActiveTickets
+	}
+
+	var load int
+
+	if tikets < 9 {
+		load = 1
+	} else if tikets <= 16 {
+		load = 2
+	} else {
+		load = 3
+	}
+
+	time := (60 / 18) * tikets
+
+	return []int{load, time}
+}
+
+func getIncidentData(resultT *ResultT) []incidentData.IncidentData {
+	incidData, err := incidentData.GetData()
+	if err != nil {
+		resultT.Error = err.Error()
+		return nil
+	}
+
+	var newIncidentData []incidentData.IncidentData
+
+	for _, i := range incidData {
+		if i.Status == "active" {
+			newIncidentData = append(newIncidentData, i)
+		}
+	}
+
+	for _, i := range incidData {
+		if i.Status == "closed" {
+			newIncidentData = append(newIncidentData, i)
+		}
+	}
+
+	return newIncidentData
 }
